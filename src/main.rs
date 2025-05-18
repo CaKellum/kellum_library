1aa0dc5d2e8de6a27349098f9fd28f706a7e9ae5
Chris Kellum <christopherk379@gmail.com>
2025-05-18 06:55:21 -0400

    refactored some of the enums and model

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::usize;
-use actix_web::{delete, error, get,
-                http::{header::ContentType, StatusCode}, 
-                middleware::{from_fn, Next}, post, put, 
-                web::{ self, Json, Path}, App, HttpResponse, HttpServer, Responder, 
-                dev::{ServiceRequest, ServiceResponse}, body::MessageBody, Error};
+use std::{usize,env};
+use actix_web::{body::MessageBody, delete, dev::{ServiceRequest, ServiceResponse}, error, get, http::{header::ContentType, StatusCode}, middleware::{from_fn, Next}, post, put, web::{ self, Json, Path}, App, Error, HttpResponse, HttpServer, Responder};
 use derive_more::derive::{Display, Error};
 use rusqlite::Connection;
 use serde::{Deserialize, Serialize};
@@ -17,25 +13,25 @@ enum PlatformType {
 }
 
 impl PlatformType {
-    fn platform_from_string(type_string: &str) -> PlatformType {
+    fn platform_from_string(type_string: &str) -> Option<PlatformType> {
         return match type_string {
-            "NES" => PlatformType::NES,
-            "SNES" => PlatformType::SNES,
-            "N64" => PlatformType::N64,
-            "GameCube" => PlatformType::GameCube,
-            "Wii" => PlatformType::Wii,
-            "WiiU" => PlatformType::WiiU,
-            "Switch" => PlatformType::Switch, 
-            "Switch2" => PlatformType::Switch2,
-            "NintendoDS" => PlatformType::NintendoDS,
-            "Nintendo3DS" => PlatformType::Nintendo3DS,
-            "Computer" => PlatformType::Computer,
-            "Playstation1" => PlatformType::Playstation1,
-            "Playstation2" => PlatformType::Playstation2, 
-            "Playstation3" => PlatformType::Playstation3, 
-            "Playstation4" => PlatformType::Playstation4,
-            "Playstation5" => PlatformType::Playstation5,
-            _ => todo!("make a non platform")
+            "NES" => Some(PlatformType::NES),
+            "SNES" => Some(PlatformType::SNES),
+            "N64" => Some(PlatformType::N64),
+            "GameCube" => Some(PlatformType::GameCube),
+            "Wii" => Some(PlatformType::Wii),
+            "WiiU" => Some(PlatformType::WiiU),
+            "Switch" => Some(PlatformType::Switch), 
+            "Switch2" => Some(PlatformType::Switch2),
+            "NintendoDS" => Some(PlatformType::NintendoDS),
+            "Nintendo3DS" => Some(PlatformType::Nintendo3DS),
+            "Computer" => Some(PlatformType::Computer),
+            "Playstation1" => Some(PlatformType::Playstation1),
+            "Playstation2" => Some(PlatformType::Playstation2), 
+            "Playstation3" => Some(PlatformType::Playstation3), 
+            "Playstation4" => Some(PlatformType::Playstation4),
+            "Playstation5" => Some(PlatformType::Playstation5),
+            _ => None 
         };
     }
 
@@ -67,14 +63,14 @@ enum ESRBRating {
 }
 
 impl ESRBRating {
-    fn rating_from_string(rating_string: &str) -> ESRBRating {
+    fn rating_from_string(rating_string: &str) -> Option<ESRBRating> {
         return match rating_string {
-            "Everyone" => ESRBRating::Everyone,
-            "Everyone10" => ESRBRating::Everyone10,
-            "Teen" => ESRBRating::Teen,
-            "Mature" => ESRBRating::Mature,
-            "AdultOnly" => ESRBRating::AdultOnly,
-            _ => todo!("make a non rating")
+            "Everyone" => Some(ESRBRating::Everyone),
+            "Everyone10" => Some(ESRBRating::Everyone10),
+            "Teen" => Some(ESRBRating::Teen),
+            "Mature" => Some(ESRBRating::Mature),
+            "AdultOnly" => Some(ESRBRating::AdultOnly),
+            _ => None
         };
     }
 
@@ -99,11 +95,45 @@ struct Game {
 }
 
 impl Game {
-   fn new(title: String, platform: &str, rating: &str, number_of_players: u8) -> Game {
-       let platform = PlatformType::platform_from_string(platform);
-       let rating = ESRBRating::rating_from_string(rating);
-       return Game { id: Uuid::new_v4().to_string(), title: title, platform: platform, rating: rating, number_of_players: number_of_players };
+   fn new(title: String, platform: &str, rating: &str, number_of_players: u8) -> Option<Game> {
+       let platform = PlatformType::platform_from_string(platform)?;
+       let rating = ESRBRating::rating_from_string(rating)?;
+       return Some(Game{ id: Uuid::new_v4().to_string(), title, platform, rating, number_of_players });
    }
+}
+
+enum MPAARating {
+    GeneralAudiences,
+    ParentalGuidance,
+    ParentsStronglyCautioned,
+    Restricted,
+    AdultsOnly
+}
+
+impl MPAARating {
+   fn from_string(rating_string: &str) -> Option<Self> {
+       match rating_string {
+           _ => None
+       }
+   }
+}
+
+enum MotionPictureFormat {
+   BluRay,
+   UltraHD,
+   DVD,
+   VHS
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Movie {
+    id: String,
+    title: String,
+    format: MotionPictureFormat,
+    rating: MPAARating
+}
+
+impl Movie { 
 }
 
 #[derive(Debug, Display, Error)]
@@ -112,6 +142,8 @@ enum ServiceError {
     ConnectionFailure,
     #[display("Failed to find a game for specified id")]
     GameNotFound,
+    #[display("Failed to make new game")]
+    FailedToMakeGame,
 }
 
 impl error::ResponseError for ServiceError {
@@ -125,6 +157,7 @@ impl error::ResponseError for ServiceError {
         return match *self {
            Self::ConnectionFailure => StatusCode::INTERNAL_SERVER_ERROR, 
            Self::GameNotFound => StatusCode::NOT_FOUND,
+           Self::FailedToMakeGame => StatusCode::IM_A_TEAPOT
         };
     }
 }
@@ -132,8 +165,8 @@ struct GameDataBase;
 impl GameDataBase {
     
     async fn get_connection() -> Result<rusqlite::Connection, ServiceError> {
-        println!("getting connection");
-        return match Connection::open("kellum_library.db") {
+        let db_path = env::var("DB_PATH").unwrap_or("kellum_library.db".to_string());
+        return match Connection::open(db_path) {
             Ok(conn) => Ok(conn),
             Err(_) => Err(ServiceError::ConnectionFailure),
         };
@@ -148,11 +181,12 @@ impl GameDataBase {
                         if let Ok(platform_string) = row.get::<usize, String>(2) { 
                             if let Ok(rating_string) = row.get::<usize, String>(3) { 
                                 if let Ok(number) = row.get::<usize, u8>(4) {
-                                    return Ok(Game{id,
-                                                   title: title.to_string(),
-                                                   platform: PlatformType::platform_from_string(&platform_string),
-                                                   rating: ESRBRating::rating_from_string(&rating_string),
+                                    if let Some(platform) = PlatformType::platform_from_string(&platform_string) {
+                                        if let Some(rating) = ESRBRating::rating_from_string(&rating_string) {
+                                            return Ok(Game{id, title: title.to_string(), platform, rating,
                                                    number_of_players: number});
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -161,7 +195,6 @@ impl GameDataBase {
                 return Err(rusqlite::Error::FromSqlConversionFailure(1, rusqlite::types::Type::Real,
                         Box::new(ServiceError::GameNotFound)));
         });
-        println!("getting game");
         let interpreted_res = match res {
             Ok(game) => Ok(Some(game)),
             Err(_) => Err(ServiceError::GameNotFound)
@@ -173,15 +206,17 @@ impl GameDataBase {
         let conn = GameDataBase::get_connection().await?;
         let mut statment = conn.prepare("SELECT id, title, platform, rating, number_of_players FROM games").unwrap();
         let games_res = statment.query_map([], |row| {
-            return Ok(Game{
-                id: row.get::<usize, String>(0)?,
-                title: row.get::<usize, String>(1)?,
-                platform: PlatformType::platform_from_string(&row.get::<usize, String>(2)?),
-                rating: ESRBRating::rating_from_string(&row.get::<usize, String>(3)?),
-                number_of_players: row.get::<usize, u8>(4)?
-            });
+                if let Some(platform) = PlatformType::platform_from_string(&row.get::<usize, String>(2)?) {
+                    if let Some(rating) = ESRBRating::rating_from_string(&row.get::<usize, String>(3)?) {
+                        return Ok(Game{ id: row.get::<usize, String>(0)?,
+                                        title: row.get::<usize, String>(1)?, 
+                                        platform, rating, 
+                                        number_of_players: row.get::<usize, u8>(4)?})
+                    }
+                }
+                return Err(rusqlite::Error::FromSqlConversionFailure(1, rusqlite::types::Type::Real,
+                        Box::new(ServiceError::GameNotFound)));
         });
-        println!("getting game");
         return match games_res {
             Ok(game_map) => {
                 let mut game_list = vec![];
@@ -203,7 +238,6 @@ impl GameDataBase {
             Some(id) => conn.execute("DELETE FROM games WHERE id=?1",[id]),
             None => conn.execute("DROP TABLE games", []) 
         };
-        println!("deleting game");
         return match result { Ok(_) => Ok(true), Err(_) => Ok(false) };
     }
 
@@ -214,7 +248,6 @@ impl GameDataBase {
                                              WHERE id=?5", 
                                              [updated_game.title, updated_game.platform.string(),
                                              updated_game.number_of_players.to_string()]);
-        println!("updating game new");
         return match statement_result { Ok(_) => Ok(true), Err(_) => Ok(false) };
     }
 
@@ -224,7 +257,6 @@ impl GameDataBase {
                                              VALUES (?1,?2,?3,?4,?5);", 
                                             [new_game.id, new_game.title, new_game.platform.string(),
                                             new_game.rating.string(),new_game.number_of_players.to_string()]);
-        println!("receving game new");
         return match statement_result { Ok(_) => Ok(true), Err(_) => Ok(false) };
     }
 }
@@ -233,9 +265,13 @@ impl GameDataBase {
 async fn add_game(new_game: Json<Game>) -> Result<impl Responder, ServiceError> {
     let real_new_game = Game::new(new_game.title.clone(), &new_game.platform.string(), 
                                   &new_game.rating.string(), new_game.number_of_players);
-    let did_insert = GameDataBase::insert_game(real_new_game).await?;
-    println!("receving game new");
-    return Ok(HttpResponse::Ok().json(did_insert));
+    if let Some(game) = real_new_game {
+        let did_insert = GameDataBase::insert_game(game).await?;
+        println!("receving game new");
+        return Ok(HttpResponse::Ok().json(did_insert));
+    } else {
+        return Err(ServiceError::FailedToMakeGame);
+    }
 }
 
 #[get("/all")]
@@ -244,7 +280,6 @@ async fn get_all_games() -> Result<impl Responder, ServiceError> {
         Some(games) => HttpResponse::Ok().json(games),
         None => HttpResponse::NotFound().body("games not Found")
     };
-    println!("game responding");
     return Ok(resp);
 }
 
@@ -255,14 +290,12 @@ async fn get_games(path: Path<(String,)>) -> Result<impl Responder, ServiceError
         Some(game) => HttpResponse::Ok().json(game),
         None => HttpResponse::NotFound().body("games not Found")
     };
-    println!("game responding");
     return Ok(resp);
 }
 
 #[put("/update")]
 async fn update_game_with(updated_game: Json<Game>) -> Result<impl Responder, ServiceError> {
     let was_updated = GameDataBase::update_game(updated_game.into_inner()).await?;
-    println!("game responding {}", was_updated);
     return Ok(HttpResponse::Ok().json(was_updated));
 }
 
@@ -270,14 +303,12 @@ async fn update_game_with(updated_game: Json<Game>) -> Result<impl Responder, Se
 async fn delete_game_with(path: Path<(String,)>) -> Result<impl Responder, ServiceError> {
     let id = path.into_inner().0;
     let was_deleted = GameDataBase::delete_game(Some(id)).await?;
-    println!("game responding {}", was_deleted);
     return Ok(HttpResponse::Ok().json(was_deleted));
 }
 
 #[delete("/remove/all")]
 async fn delete_all_games() -> Result<impl Responder, ServiceError> {
     let was_deleted = GameDataBase::delete_game(None).await?;
-    println!("game responding {}", was_deleted);
     return Ok(HttpResponse::Ok().json(was_deleted));
 }
 
@@ -290,13 +321,17 @@ async fn my_middleware(req: ServiceRequest, next: Next<impl MessageBody>,) -> Re
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
-        let scope = web::scope("/game")
+        let game_scope = web::scope("/game")
             .service(add_game)
             .service(get_all_games)
             .service(get_games)
             .service(update_game_with)
             .service(delete_game_with)
             .service(delete_all_games);
-        App::new().service(scope).wrap(from_fn(my_middleware))
+        let movie_scope = web::scope("/movie");
+        App::new()
+            .service(game_scope)
+            .service(movie_scope)
+            .wrap(from_fn(my_middleware))
     }).bind(("127.0.0.1", 8080))?.run().await
 }
