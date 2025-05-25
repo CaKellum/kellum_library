7eeb4055997183241241fcbaffbe7014a85f082a
Chris Kellum <christopherk379@gmail.com>
2025-05-25 07:41:33 -0400

    updated schema to include movies

--- schema.sql
+++ schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE games (
     rating TEXT,
     number_of_players INTEGER
 );
+
+CREATE TABLE movies (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    format TEXT,
+    rating TEXT
+);
+
